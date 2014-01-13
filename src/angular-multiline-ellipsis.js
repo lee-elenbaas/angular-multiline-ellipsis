226e968267a241a3acf013342399af8fd79e33ec
Lee Elenbaas <lee.elenbaas@perigen.com>
2014-01-13 17:45:37 +0200

    Move the transclude service up
    Attempt to move the transclude service in order to use the same
    transclude service that is used for the ngTransclude itself

--- src/angular-multiline-ellipsis.js
+++ src/angular-multiline-ellipsis.js
@@ -12,6 +12,20 @@
 					ellipsisUrl: '=',
 					ellipsisStyle: '='
 				},
+				controller: ['$element', '$transclude', function($element, $transclude) {
+					if (!$transclude) {
+					  throw minErr('ngTransclude')('orphan',
+						  'Illegal use of ngTransclude directive in the template! ' +
+						  'No parent directive that requires a transclusion found. ' +
+						  'Element: {0}',
+						  startingTag($element));
+					}
+
+					// remember the transclusion fn but call it during linking so that we don't process transclusion before directives on
+					// the parent element even when the transclusion replaces the current element. (we can't use priority here because
+					// that applies only to compile fns and not controllers
+					this.$transclude = $transclude;
+				}],
 				template: ''
 					+ '<div class="ml-ellipsis">'
 						+ '<div class="ml-ellipsis-before"></div>'
@@ -19,33 +33,20 @@
 						+ '<div class="ml-ellipsis-after">'
 							+ '<span ng-if="!ellipsis && !ellipsisUrl" ng-style="ellipsisStyle">&hellip;</span>'
 							+ '<span ng-if="ellipsis" ng-bind="ellipsis" ng-style="ellipsisStyle"></span>'
-							+ '<span ng-if="!ellipsis && ellipsisUrl" exclude-from-isolated>'
+							+ '<span ng-if="!ellipsis && ellipsisUrl" ml-ellipsis-exclude-from-isolated>'
 								+ '<span ng-include="ellipsisUrl" ng-style="ellipsisStyle"></span>'
 							+ '</span>'
 						+ '</div>'
 					+ '</div>'
 			};
 		})
-		.directive('excludeFromIsolated', function() {
+		.directive('mlEllipsisExcludeFromIsolated', function() {
 		  return {
 			restrict: "EA",
+			require: '^mlEllipsis'
 			transclude: true,
-			controller: ['$element', '$transclude', function($element, $transclude) {
-				if (!$transclude) {
-				  throw minErr('ngTransclude')('orphan',
-					  'Illegal use of ngTransclude directive in the template! ' +
-					  'No parent directive that requires a transclusion found. ' +
-					  'Element: {0}',
-					  startingTag($element));
-				}
-
-				// remember the transclusion fn but call it during linking so that we don't process transclusion before directives on
-				// the parent element even when the transclusion replaces the current element. (we can't use priority here because
-				// that applies only to compile fns and not controllers
-				this.$transclude = $transclude;
-			}],
 			link: function($scope, $element, $attrs, controller) {
-				controller.$transclude(scope.$parent, function(clone) {
+				controller.$transclude(function(clone) {
 				  $element.empty();
 				  $element.append(clone);
 				});
